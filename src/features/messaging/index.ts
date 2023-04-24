249cea89a3ec441c3094f176945570de15fc684c
Enzo Soares <enzo.soares@galitt.com>
2023-04-24 11:49:24 +0200

    :sparkles: adding messaging route

---
+++ src/features/messaging/index.ts
@@ -0,0 +1,1 @@
+export * from "./Messaging.Route.tsx";
--- src/features/routing/Routing.Router.tsx
+++ src/features/routing/Routing.Router.tsx
@@ -1,8 +1,10 @@
 import {Route, Routes} from "react-router-dom";
 import {LoginRoute} from "../login";
+import {MessagingRoute} from "../messaging";
 
 export function RoutingRouter() {
   return <Routes>
     <Route path="login" element={<LoginRoute/>}/>
+    <Route path="messaging" element={<MessagingRoute/>}/>
   </Routes>;
 }
