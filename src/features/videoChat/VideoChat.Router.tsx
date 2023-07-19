4a3fd63d4f88ae0520bede8a095c08cdf326024a
Denis Turbiez <denisft77@gmail.com>
2023-07-20 00:37:00 +0200

    WebRTC

--- src/features/videoChat/VideoChat.Router.tsx
+++ src/features/videoChat/VideoChat.Router.tsx
@@ -6,12 +6,23 @@ import {
   CameraContext, Form, useCamera
 } from "../../lib";
 
+const ws = new WebSocket("ws://api.flash-player-revival.net/webrtc");
+
 export function VideoChatRouter() {
   const video = useRef<HTMLVideoElement>(null);
+  const video2 = useRef<HTMLVideoElement>(null);
   const { isLoading, stream } = useCamera();
   const {
     useVideo, switchUseVideo, useAudio, switchUseAudio
   } = useContext(CameraContext);
+
+  ws.onopen = function (ev) {
+    console.log(ev);
+  };
+
+  ws.onerror = function (err) {
+    console.log(err);
+  };
 
   const servers: RTCConfiguration = {
     iceServers: [
@@ -24,30 +35,68 @@ export function VideoChatRouter() {
     ]
   };
   const peerConnection = new RTCPeerConnection(servers);
-  peerConnection.onicecandidate = e => console.log("event", e?.candidate);
 
-  const addStreams = (tracks: MediaStreamTrack[]) => {
-    tracks.forEach(track => peerConnection.addTrack(track));
 
+  peerConnection.oniceconnectionstatechange = () => {
+    console.log(peerConnection.iceConnectionState);
+  };
+
+  const addStreams = (stream: MediaStream) => {
+    console.log("Track added");
+    stream.getTracks().forEach(track => {
+      console.log(peerConnection.addTrack(track, stream));
+    });
+
+    console.log("Create offer");
     // eslint-disable-next-line max-len
-    peerConnection.createOffer().then(offer => peerConnection.setLocalDescription(offer).then(() => console.log("offer", offer)));
+    peerConnection.createOffer().then(offer => peerConnection.setLocalDescription(offer));
   };
 
-  const addConnection = (candidate: string) => {
-    console.log(candidate);
-    peerConnection.setRemoteDescription(new RTCSessionDescription({ type: "offer", sdp: candidate })).then(
-      () => peerConnection.createAnswer().then(answer => {
-        peerConnection.setLocalDescription(answer);
-        console.log(answer);
-      })
-    );
+  peerConnection.ontrack = ({streams: [stream]}) => {
+    console.log("stream");
+    if(video2.current !== null) {
+      console.log(stream);
+      video2.current.srcObject = stream;
+      video2.current?.play();
+    }
   };
+
+  const callUser = async () => {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+    ws.send(JSON.stringify(offer));
+    console.log("send call");
+  };
+
+  ws.onmessage = async function (message: MessageEvent<string>) {
+    const data: RTCSessionDescriptionInit = JSON.parse(message.data);
+    if(data.type === "offer"){
+      console.log("REceive offer");
+      await peerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(message.data)));
+      const answer = await peerConnection.createAnswer();
+      peerConnection.setLocalDescription(answer);
+      ws.send(JSON.stringify(answer));
+    }else if(data.type === "answer"){
+      console.log("receive answer");
+      peerConnection.setRemoteDescription(data);
+    }
+  };
+
+  // const addConnection = (candidate: string) => {
+  //   console.log(candidate);
+  //   peerConnection.setRemoteDescription(new RTCSessionDescription({ type: "offer", sdp: candidate })).then(
+  //     () => peerConnection.createAnswer().then(answer => {
+  //       peerConnection.setLocalDescription(answer);
+  //       console.log(answer);
+  //     })
+  //   );
+  // };
 
   useEffect(() => {
     if (video.current && stream ) {
       video.current.srcObject = stream;
       video.current.play();
-      addStreams(stream.getTracks());
+      addStreams(stream);
     }
   }, [stream, video]);
 
@@ -56,6 +105,7 @@ export function VideoChatRouter() {
     <button onClick={switchUseVideo}>{useVideo ? "disable video" : "enable video"}</button>
     <button onClick={switchUseAudio}>{useAudio ? "disable audio" : "enable audio"}</button>
     {(useVideo && stream) && <video ref={video}/>}
+    <video ref={video2}/>
     <Form submitButtonText="initier connection"
       schemas={[
         {
@@ -65,7 +115,7 @@ export function VideoChatRouter() {
           required: true
         }
       ]}
-      onSubmit={e => addConnection(e["ip"] ?? "")}
+      onSubmit={() => callUser()}
     />
   </>;
 
