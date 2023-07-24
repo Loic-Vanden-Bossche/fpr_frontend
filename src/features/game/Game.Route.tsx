27eb979683531fa72d16fa9cb1f1eebfa6a9ee71
Denis Turbiez <denisft77@gmail.com>
2023-07-24 12:05:11 +0200

    Remove test

--- src/features/game/Game.Route.tsx
+++ src/features/game/Game.Route.tsx
@@ -1,6 +1,6 @@
-import { gameExample } from "../../types";
-import { Game2DEngine } from "./Game.2DEngine";
+// import { gameExample } from "../../types";
+// import { Game2DEngine } from "./Game.2DEngine";
 
 export function GameRoute() {
-  return <Game2DEngine game={gameExample}/>;
+  // return <Game2DEngine game={gameExample}/>;
 }
