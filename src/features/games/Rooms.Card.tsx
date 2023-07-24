c844de089bb8c60fe6f801231e61aba9d8f1f467
Denis Turbiez <denisft77@gmail.com>
2023-07-24 09:33:40 +0200

    Add status to room card

--- src/features/games/Rooms.Card.tsx
+++ src/features/games/Rooms.Card.tsx
@@ -1,10 +1,12 @@
 import { Room } from "../../types/Room.ts";
-import { css } from "@emotion/react";
+import { css, jsx } from "@emotion/react";
 import { gameCard } from "./Games.style.ts";
 import { outWhiteShadow } from "../../ui";
 import { useContext } from "react";
 import { gameStompSocket } from "../../ws/gaming.ts";
 import { useNavigate } from "react-router-dom";
+import { icons } from "../../lib";
+import JSX = jsx.JSX;
 
 interface Props {
   room: Room
@@ -14,6 +16,15 @@ export function RoomsCard({ room }: Props) {
   const gaming = useContext(gameStompSocket);
   const navigate = useNavigate();
 
+  let status: JSX.Element;
+  if(room.status === "STARTED") {
+    status = icons.play;
+  } else if(room.status === "WAITING"){
+    status = icons.wait;
+  } else {
+    status = <p/>;
+  }
+
   return <div css={css(gameCard(room.game.id), outWhiteShadow)} onClick={() => {
     gaming.onConnect = () => {
       gaming.publish({ destination: "/app/joinRoom/" + room.id });
@@ -21,6 +32,7 @@ export function RoomsCard({ room }: Props) {
     };
     gaming.activate();
   }}>
+    <p style={{ position: "absolute", top: "8px", right: "8px" }}>{status}</p>
     <h2>{room.game.title}</h2>
     <p>Players : {room.players.length}</p>
   </div>;
